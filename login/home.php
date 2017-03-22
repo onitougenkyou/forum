758cd5324ee6c7851be20867a7702eb2a94bcd93
LAPTOP-NIJDM9S8\death <laurent.chauveau@yahoo.fr>
2017-03-22 09:27:42 +0100

    Change in fr

--- login/home.php
+++ login/home.php
@@ -21,15 +21,12 @@ $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
 <body>
 
 <div class="header">
- <div class="left">
-     <label><a href="http://www.codingcage.com/">Coding Cage - Programming Blog</a></label>
-    </div>
     <div class="right">
      <label><a href="logout.php?logout=true"><i class="glyphicon glyphicon-log-out"></i> logout</a></label>
     </div>
 </div>
 <div class="content">
-welcome : <?php print($userRow['user_name']); ?>
+Bienvenue : <?php print($userRow['user_name']); ?>
 </div>
 </body>
 </html>
