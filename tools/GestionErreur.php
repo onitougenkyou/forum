b865a87d64e57091052ff44296b282809f7a4c2d
84156468 <juzywotok@laposte.net>
2017-03-22 16:02:29 +0100

    update Affichage
    Ajout d'un h1

--- tools/GestionErreur.php
+++ tools/GestionErreur.php
@@ -27,15 +27,25 @@ class GestionErreur
 	/**
 	*	ERROR PDO
 	*
-	* @param no			Type d'erreur, E_USER_NOTICE, E_USER_WARNING, E_USER_ERROR
+	* @param no		Type d'erreur, E_USER_NOTICE, E_USER_WARNING, E_USER_ERROR
 	* @param str		Message d'erreur
-	* @param file		option - le nom du fichier dans laquelle l'erreur a été rencontrée
-	* @param line		option - la ligne de l'erreur dans le fichier
+	* @param file	option - le nom du fichier dans laquelle l'erreur a été rencontrée
+	* @param line	option - la ligne de l'erreur dans le fichier
 	**/
 	function erreurPDO($no, $str, $file, $line)
 	{
-		echo '<p>Erreur ['.$no.'] : '.$str.'<br/>';
-		echo 'Survenue dans le fichier : "'.$file.'" à la ligne '.$line.'.</p>';
+		$strReturn = '<p class="erreur">';
+			$strReturn .= '<h1>ERREUR !!</h1>';
+			$strReturn .= 'Erreur ['.$no.'] =>> <b>'.$str.'</b><br>';
+			$strReturn .= 'Survenue dans le fichier : "<b>'.$file.'</b>" à la ligne <b>'.$line.'</b><br>';
+			$strReturn .= '<i>Ceci était un message d\'erreur personnalité de PDO via la classe "GestionErreur"</i>';
+		$strReturn .= '</p>';
+		// Affichage de l'erreur
+		echo $strReturn;
 	}
 
 }
+
+
+
+
