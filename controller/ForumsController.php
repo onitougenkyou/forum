b0363e48324a1770feede891fadd8ce799e3ebcb
84156468 <juzywotok@laposte.net>
2017-03-23 12:00:47 +0100

    Création de Controller
    ForumController renommé ForumsController
    Création de SujetController
    Création de MessageController

---
+++ controller/ForumsController.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+*
+*	Forums Controleur
+*		Controleur de la partie Forum du site
+*
+* @user Cedric
+* @date 2017.03.23
+**/
+class ForumsController
+{
+	private $db;
+	
+	/**
+	*	Constructeur
+	*
+	**/
+	public function __construct($db)
+	{
+		$this->db = $db;
+	}
+	
+	
+	
+}
