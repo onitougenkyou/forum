129b7946b76d73bb4f908ee968ae522ada18c049
LAPTOP-NIJDM9S8\death <laurent.chauveau@yahoo.fr>
2017-03-27 10:08:15 +0200

    base user à jour :D

--- sql/users.sql
+++ sql/users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Mar 21 Mars 2017 à 10:57
+-- Généré le :  Lun 27 Mars 2017 à 08:07
 -- Version du serveur :  5.7.14
 -- Version de PHP :  5.6.25
 
@@ -30,15 +30,22 @@ CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
   `user_name` varchar(255) NOT NULL,
   `user_email` varchar(60) NOT NULL,
-  `user_pass` varchar(255) NOT NULL
+  `user_pass` varchar(255) NOT NULL,
+  `user_role` text,
+  `user_name_family` varchar(30) DEFAULT NULL,
+  `user_date` date DEFAULT NULL,
+  `user_description` varchar(250) DEFAULT NULL,
+  `user_avatar` text
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `users`
 --
 
-INSERT INTO `users` (`user_id`, `user_name`, `user_email`, `user_pass`) VALUES
-(1, 'Test', 'test@gmail.com', '$2y$10$LlgsQbj36yYg0VXRTKxfY.Xidy/fJ7W0Ws.apCqf.eVktVwjc570K');
+INSERT INTO `users` (`user_id`, `user_name`, `user_email`, `user_pass`, `user_role`, `user_name_family`, `user_date`, `user_description`, `user_avatar`) VALUES
+(35, 'Laurent', 'laurent@gmail.com', '$2y$10$2mnschIE2B7jcTuCQVRkkOdIHPgQEbHJFfAO11fpuoCoo/O2x2PUe', 'super-admin', NULL, NULL, NULL, NULL),
+(36, 'david', 'droulier@gmail.com', '$2y$10$GZiGiQ4QvanlBdng6ye/hewegL0Xz0pvWIOLHELS82GPFBiH1DMXO', NULL, NULL, NULL, NULL, NULL),
+(37, 'lolo', 'lolo@gmail.com', '$2y$10$to.dSHakjkVPSZg2n1AQDef5WGSB1axnwubvsTQ47bKmOKYFnVt1e', NULL, NULL, NULL, NULL, NULL);
 
 --
 -- Index pour les tables exportées
@@ -60,7 +67,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
