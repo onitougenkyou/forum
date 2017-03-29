a596c512c08722de1e712d471e82f429b716a8b4
LAPTOP-NIJDM9S8\death <laurent.chauveau@yahoo.fr>
2017-03-29 16:46:17 +0200

    création chat.php (test encore)

--- chat/chat.php
+++ chat/chat.php
@@ -1,8 +1,71 @@
-<?php
-$nom = $_POST['nom'];                    //On récupère le pseudo et on le stocke dans une variable
-$message = $_POST['message'];            //On fait de même avec le message
-$ligne = $nom.' > '.$message.'<br>';     //Le message est créé
-$leFichier = file('ac.htm');             //On lit le fichier ac.htm et on stocke la réponse dans une variable (de type tableau)
-array_unshift($leFichier, $ligne);       //On ajoute le texte calculé dans la ligne précédente au début du tableau
-file_put_contents('ac.htm', $leFichier); //On écrit le contenu du tableau $leFichier dans le fichier ac.htm
-?>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Chat jQuery</title>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <style type="text/css">
+  #conversation {
+    width: 300px;
+    height: 400px;
+    border: black 1px solid;
+    background-color: #efecca;
+    overflow-x: hidden;
+    overflow-y: scroll;
+    padding: 5px;
+    margin-left: 10px;
+  }
+  fieldset {
+    width: 330px;
+    background-color: #e6e2af;
+    border: black 1px solid;
+  }
+  </style>
+</head>
+
+<body>
+  <?php include ('../header.html'); ?>
+  <fieldset>
+    <legend>Chat</legend>
+    <div id="conversation"></div><br />
+    <form action="#" method="post">
+      <input type="text" id="message" size="27">
+      <button type="button" id="envoyer" title="Envoyer">Send</button>
+    </form>
+  </fieldset>
+  <script>
+  $(function() {
+    afficheConversation();
+
+    $('#envoyer').click(function() {
+      var nom = $('#nom').val();
+      var message = $('#message').val();
+      var date = new Date();
+      heure = date.getHours();
+      min = date.getMinutes();
+      seconde = date.getSeconds();
+      $.post('donneesChat.php', {
+        'nom': nom,
+        'date': heure, min, seconde,
+        'message': message
+      }, afficheConversation);
+    });
+
+    function longueur() {
+      if (  $('#message').val() > 25) {
+        return false;
+      }
+
+    }
+
+    function afficheConversation() {
+      $('#conversation').load('chat.txt');
+      $('#message').val('');
+      $('#message').focus();
+    }
+
+    setInterval(afficheConversation, 600000);
+  });
+  </script>
+
+</body>
+</html>
