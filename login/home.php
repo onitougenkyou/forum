c8f08d4a2026b76ed264f5c074b4916b86c85c82
Argetkill\HarleyDav <flo.pouyer@hotmail.fr>
2017-03-29 16:38:02 +0200

    Changement session suite

--- login/home.php
+++ login/home.php
@@ -1,13 +1,12 @@
 <?php
+
+
 include_once 'verif.php';
 if(!$user->is_loggedin())
 {
  $user->redirect('../login.php');
 }
-$user_id = $_SESSION['user_session'];
-$stmt = $DB_con->prepare("SELECT * FROM users WHERE user_id=:user_id");
-$stmt->execute(array(":user_id"=>$user_id));
-$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -15,7 +14,7 @@ $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css"  />
 <link rel="stylesheet" href="style.css" type="text/css"  />
-<title>Bienvenue - <?php print($userRow['user_email']); ?></title>
+<title>Bienvenue - <?php print($user->data['user_email']); ?></title>
 </head>
 
 <body>
@@ -26,7 +25,7 @@ $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
     </div>
 </div>
 <div class="content">
-Bienvenue : <?php print($userRow['user_name']); ?>
+Bienvenue : <?php print($user->data['user_name']); ?>
 </div>
 <div class="profil-button">
   <a href="../template/profil_vu.php">Accéder à votre profil</a>
