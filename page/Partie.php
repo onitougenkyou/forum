5dd7868b8716ecb1fb4752cb9681402ff8c535e9
LAPTOP-NIJDM9S8\death <laurent.chauveau@yahoo.fr>
2017-03-29 16:49:11 +0200

    changement place

---
+++ page/Partie.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> Partie </title>
+  </head>
+  <body>
+    <?php include ('../header.html'); ?>
+
+    <div class="block-text">
+       DESCRIPTION</br>   <!-- en maj et assez gros -->
+      <p>Le <strong> jeu de rôle </strong> est une technique ou activité, par laquelle une personne interprète le rôle d’un personnage
+      (réel ou imaginaire) dans un environnement fictif. Le participant agit à travers ce rôle par des actions physiques,
+      par des actions narratives (dialogues improvisés, descriptions) ou par des prises de décision sur le développement du personnage.</p>
+      <p>Le <strong> jeu de rôle </strong>« traditionnel » ou « sur table », est un jeu de société inventé au milieu des années 1970. Dans ce jeu,
+      les participants décrivent par la parole les actions de leur personnage, en se basant sur des règles de jeu.
+      Cette forme de jeu est jouée traditionnellement autour d’une table (avec dés, feuilles et crayon)</p>
+    </div>
+    <div class="block-image">
+      NOS JEUX DE ROLE ACTUEL</br> <!-- meme chose -->
+      <img src="https://rolistetv.com/wp-content/uploads/2015/02/hex_cover.gif" class="hollow earth">
+      <a href="page/HollowEarthExpedition.php">Hollow earth expedition</br>
+      <img src="https://vignette3.wikia.nocookie.net/donjons-et-dragons/images/2/20/D%26d.png/revision/latest?cb=20100724003108&path-prefix=fr" class="Donjon et dragon">
+      <a href="page/DonjonEtDragon.php">Donjon et dragon</br>
+      <img src="http://c7.alamy.com/comp/D21CHT/die-miami-cops-miami-super-cops-bud-spencer-terence-hill-fbi-agenten-D21CHT.jpg" class="Super cops">
+      <a href="page/SuperCops.php">Super cops</br>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsmJphe11H_HNduyBOCLK4Qhbf7hH2c2HIqddnOqJbEVYi7FP9" class="Zcorps">
+      <a href="zCorps.php">Zcorps</br>
+    </div>
+  </body>
+</html>
