b73356d65952c8477c7aee4501be5921ea62ce8d
LAPTOP-NIJDM9S8\death <laurent.chauveau@yahoo.fr>
2017-04-03 09:24:22 +0200

    Fichier déplacer

--- view/site/header.php
+++ view/site/header.php
@@ -6,16 +6,15 @@
   <link href="https://fonts.googleapis.com/css?family=Macondo" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="../css/headStyle.css" type="text/css"  />
-
-  <link rel="stylesheet" href="../css/tchat.css" type="text/css"  />
-  <link rel="stylesheet" href="../css/profilVuStyle.css" type="text/css"  />
+  <link rel="stylesheet" href="css/headStyle.css" type="text/css"  />
+  <link rel="stylesheet" href="css/tchat.css" type="text/css"  />
+  <link rel="stylesheet" href="css/profilVuStyle.css" type="text/css"  />
 
 </head>
 <body>
 
   <div class="head-picture">
-    <img src="../imagesForum/demon.jpg" class ="img-responsive" alt="demon">
+    <img src="imagesForum/demon.jpg" class ="img-responsive" alt="demon">
   </div>
   <nav class="navbar navbar-default">
     <div class="container-fluid">
@@ -36,7 +35,7 @@
             <a href="#" id="btn-Menu" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu <span class="caret"></span></a>
             <ul class="dropdown-menu">
               <li><a href="../page/partie.php"><label id="jdr">JDR</label></a></li>
-              <li><a href="#"><label id="forum">Forum</label></a></li>
+              <li><a href="../index.forum.php"><label id="forum">Forum</label></a></li>
               <li><a href="facebook.com"><label id="facebook">Facebook</label></a></li>
               <li><a href="#"><label id="contact">contactez</label></a></li>
               <li><a href="../chat/chat.php"><label id="tchat">tchat</label></a></li>
@@ -46,13 +45,13 @@
         </ul>
         <ul class="nav navbar-nav navbar-right">
           <li><a id="profil" href="../template/profil_vu.php"> Profil</a></li>
-          <li><a id ="btn-logout" class="navbar-brand deco" href="..\login\logout.php">logout</a></li>
+          <li><a id ="btn-logout" class="navbar-brand deco" href="login/logout.php">logout</a></li>
         </ul>
       </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
   </nav>
   <div id = "crane">
-    <img src="../imagesForum/navbar/tete.png" class="img-responsive" alt="crane">
+    <img src="imagesForum/navbar/tete.png" class="img-responsive" alt="crane">
   </div>
 
  <!-- <div id= "coloneD">
