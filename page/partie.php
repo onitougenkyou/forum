ff3f468e742c246f3b4e29f4e18d569993443b1b
LAPTOP-NIJDM9S8\death <laurent.chauveau@yahoo.fr>
2017-04-03 09:23:21 +0200

    Ajout bootstrap

--- page/partie.php
+++ page/partie.php
@@ -1,32 +1,46 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title> Partie </title>
-  </head>
-  <body>
-    <?php include ('../header.php'); ?>
-    <div class="block-text">
-       DESCRIPTION</br>   <!-- en maj et assez gros -->
-      <p>Le <strong> jeu de rôle </strong> est une technique ou activité, par laquelle une personne interprète le rôle d’un personnage
-      (réel ou imaginaire) dans un environnement fictif. Le participant agit à travers ce rôle par des actions physiques,
-      par des actions narratives (dialogues improvisés, descriptions) ou par des prises de décision sur le développement du personnage.</p>
-      <p>Le <strong> jeu de rôle </strong>« traditionnel » ou « sur table », est un jeu de société inventé au milieu des années 1970. Dans ce jeu,
-      les participants décrivent par la parole les actions de leur personnage, en se basant sur des règles de jeu.
-      Cette forme de jeu est jouée traditionnellement autour d’une table (avec dés, feuilles et crayon)</p>
-    </div>
-    <div class="block-image">
-      NOS JEUX DE ROLE ACTUEL</br> <!-- meme chose -->
+<head>
+  <meta charset="utf-8">
+  <title> Partie </title>
+  <link rel="stylesheet" href="login/pageStyle.css" type="text/css"  />
 
-      <img src="../imagesForum/pagesJdr/partieHollowEarthExpedition.gif" class="hollow earth">
-      <a href="hollowEarthExpedition.php">Hollow earth expedition</br>
-      <img src="../imagesForum/pagesJdr/partieDonjonEtDragon.png" class="Donjon et dragon">
-      <a href="donjonEtDragon.php">Donjon et dragon</br>
-      <img src="../imagesForum/pagesJdr/partieSuperCops.jpg" class="Super cops">
-      <a href="superCops.php">Super cops</br>
-      <img src="../imagesForum/pagesJdr/partieZcorps.jpeg" class="Zcorps">
-
-      <a href="zCorps.php">Zcorps</br>
-    </div>
-  </body>
-</html>
+</head>
+<body>
+  <?php include ('../header.php'); ?>
+  <div class="block-infos">
+    <div class="container">
+      <div class="row">
+        <h2>DESCRIPTION</h2>
+        <div class="col-xs-12 col-lg-8">
+          <p>Le <strong> jeu de rôle </strong> est une technique ou activité, par laquelle une personne interprète le rôle d’un personnage
+            (réel ou imaginaire) dans un environnement fictif. Le participant agit à travers ce rôle par des actions physiques,
+            par des actions narratives (dialogues improvisés, descriptions) ou par des prises de décision sur le développement du personnage.</p>
+            <p>Le <strong> jeu de rôle </strong>« traditionnel » ou « sur table », est un jeu de société inventé au milieu des années 1970. Dans ce jeu,
+              les participants décrivent par la parole les actions de leur personnage, en se basant sur des règles de jeu.
+              Cette forme de jeu est jouée traditionnellement autour d’une table (avec dés, feuilles et crayon)</p>
+            </div>
+          </div>
+          <div class="row">
+            <h3>  NOS JEUX DE ROLE ACTUEL</h3>
+            <div class="col-xs-12">
+              <img src="../imagesForum/pagesJdr/partieHollowEarthExpedition.gif" class="img-responsive hollow earth">
+              <label><a href="hollowEarthExpedition.php">Hollow earth expedition</label>
+              </div>
+              <div class="col-xs-12">
+                <img src="../imagesForum/pagesJdr/partieDonjonEtDragon.png" class="img-responsive Donjon et dragon">
+                <label><a href="donjonEtDragon.php">Donjon et dragon</label>
+                </div>
+                <div class="col-xs-12">
+                  <img src="../imagesForum/pagesJdr/partieSuperCops.jpg" class="img-responsive Super cops">
+                  <label>  <a href="superCops.php">Super cops</label>
+                  </div>
+                  <div class="col-xs-12">
+                    <img src="../imagesForum/pagesJdr/partieZcorps.jpeg" class="img-responsive Zcorps">
+                    <label for=""><a href="zCorps.php">Zcorps</label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </body>
+            </html>
