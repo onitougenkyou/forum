4d90a7e4fc70a7429e114c4692ffc44434ab8cb6
Argetkill\HarleyDav <flo.pouyer@hotmail.fr>
2017-04-03 14:12:36 +0200

    no message

---
+++ template/profil_vu.php
@@ -0,0 +1,34 @@
+<?php
+include_once '../login/verif.php';
+if($user->is_loggedin() == false)
+{
+  header ('location: ../login.php');
+}
+
+if (empty($_GET['id']) or !is_numeric($_GET['id'])){
+  include '../profil.user/erreur_profil.php';
+  include ('../header.php');
+}
+
+
+
+?>
+
+<div id="vuProfil"class="container profil">
+  <div class="row">
+    <div class="col-xs-12">
+      <div class="infos-profil">
+
+        <?php print($user->data['user_avatar']); ?><br>
+        <label>Email : <?php print($user->data['user_email']); ?> </label> <br>
+        <label>Prénom : <?php print($user->data['user_name']); ?></label>  <br>
+        <label>Nom : <?php print($user->data['user_name_family']); ?></label> <br>
+        <label>Date de naissance : <?php print($user->data['user_date']); ?></label>  <br>
+        <label>Description : <?php print($user->data['user_description']); ?></label> <br>
+      </div>
+      <div class="infos-button">
+        <button id="btn-modif" type="button" name="button-infos"><a href="../profil.user/change_infos_user.php">Modification</a></button>
+      </div>
+    </div>
+  </div>
+</div>
