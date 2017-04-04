1589736bd9a4adf529d9399a3ba1162e26ae0b45
LAPTOP-NIJDM9S8\death <laurent.chauveau@yahoo.fr>
2017-04-04 11:14:47 +0200

    chat/chat.php supprimer (obsolète)

--- chat/chat.php
+++
@@ -1,6 +0,0 @@
-    <script type="text/javascript">
-    $(document).ready(function(){
-      $.chat({$user: '<?php  print($user->data['user_name'])  ?>'}).appendTo(document.body);
-    })
-
-    </script>
